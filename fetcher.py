d9b7a0f9eb1b5e8c157a502eb9e1d207d063278b
HackR27 <HackR27@users.noreply.github.com>
2021-06-14 08:40:00 +0530

    Added sorting to the retrieved server list

--- fetcher.py
+++ fetcher.py
@@ -2,9 +2,16 @@
 ## fetches the config files and descriptions from the vpngate API
 import requests
 import pandas as pd
+import numpy as np
 from io import StringIO
 
 def getTable():
     res = requests.get('http://www.vpngate.net/api/iphone/')
-    data = pd.read_csv(StringIO(res.text[15:]))
-    return data
+    df = pd.read_csv(StringIO(res.text[15:])).fillna('9999')
+    df = df[df.Ping != '-']
+    df['Ping'] = df['Ping'].apply(pd.to_numeric)
+    return df.sort_values(by=['Ping','Speed','TotalUsers'],ascending=[1,0,1])
+    
+
+# df = getTable()
+# print(df[['IP','Speed','Ping','TotalUsers']].head())
